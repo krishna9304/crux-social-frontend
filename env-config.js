95cfcb969cd21a1017d5c5cfe3edea1d0bb1cf61
krishna9304 <krishhtrishh9304@gmail.com>
2021-07-09 20:05:52 +0530

    deploy error resolved

--- env-config.js
+++ env-config.js
@@ -1,6 +1,6 @@
 const prod = process.env.NODE_ENV === "production";
 module.exports = {
   "process.env.BACKEND_URL": prod
-    ? "https://crux-social-api.herokuapp.com/"
+    ? "https://crux-social-api.herokuapp.com"
     : "https://localhost:8080",
 };
